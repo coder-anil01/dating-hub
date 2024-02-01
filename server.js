6a15863abdbbbdc01bd8a99bc5bb2103ad94ecac
coder-anil01 <145540918+coder-anil01@users.noreply.github.com>
2024-02-01 08:33:18 +0530

    error

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const app = express();
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 app.use(cors());
-app.use(express.json());
 app.use(morgan('dev'));
 
 const PORT = process.env.PORT || 8000
@@ -31,7 +30,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
   });
   
 
